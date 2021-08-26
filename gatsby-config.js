1fc8132dd3babb373f87689d688b3596aabd0a83
Sudarshan Suryaprakash <sudarshan16rms@gmail.com>
2021-08-26 12:16:05 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "my-first-site",
+  },
+  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-gatsby-cloud"],
+};
