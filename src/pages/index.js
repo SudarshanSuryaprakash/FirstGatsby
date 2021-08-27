f394334f200ed28a9d7944420367ec89741d8bbd
Sudarshan Suryaprakash <sudarshan16rms@gmail.com>
2021-08-27 05:46:22 +0100

    react-wheel-of-prizes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,44 @@ import * as React from 'react'
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
 
+import WheelComponent from 'react-wheel-of-prizes'
+import 'react-wheel-of-prizes/dist/index.css'
+
 const IndexPage = () => {
+  const segments = [
+    'Rhys',
+    'Harry',
+    'Stef',
+    'Jon',
+    'Dan',
+    'Ellie',
+  ]
+  const segColors = [
+    '#EE4040',
+    '#F0CF50',
+    '#815CD1',
+    '#3DA5E0',
+    '#34A24F',
+    '#F9AA1F',
+  ]
+  const onFinished = (winner) => {
+    console.log(winner, ' wins')
+  }
   return (
+  <div>
+    <WheelComponent
+      segments={segments}
+      segColors={segColors}
+      winningSegment='won 10'
+      onFinished={(winner) => onFinished(winner)}
+      primaryColor='black'
+      contrastColor='white'
+      buttonText='Spin'
+      isOnlyOnce={false}
+      size={290}
+      upDuration={100}
+      downDuration={1000}
+    />
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
@@ -11,6 +47,7 @@ const IndexPage = () => {
         src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
       />
     </Layout>
+  </div>
   )
 }
 
